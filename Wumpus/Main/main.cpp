5e76af2296bce394127b221f512e385dda7ee6f3
kirukib <39464098+kirukib@users.noreply.github.com>
2020-04-28 11:37:47 +0300

    Update main.cpp

--- Wumpus/Main/main.cpp
+++ Wumpus/Main/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <world.cpp>
 
+
+using namespace wumpus_project;
+using namespace std;
+
+int main()
+{
+	World wr;
+    wr.outputGameInfo;
+    wr.printGameInfo;
+
+	return 0;
+}
